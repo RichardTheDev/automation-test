cc4c1ed3a00af10b17ce1b980f1c2376c4aee5a7
RichardTheDev <117979170+RichardTheDev@users.noreply.github.com>
2023-12-24 14:34:15 +0200

    add conserto

--- Hello.py
+++ Hello.py
@@ -1,51 +1,63 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
+import asyncio
+import aiohttp
 import streamlit as st
-from streamlit.logger import get_logger
+from PyPDF2 import PdfReader
+import io
 
-LOGGER = get_logger(__name__)
+# Replace with your actual OpenAI API key
+api_key = st.secrets["openaikey"]
 
+async def process_text(text, prompt):
+    try:
+        async with aiohttp.ClientSession() as session:
+            payload = {
+                'model': 'gpt-3.5-turbo',
+                'messages': [
+                    {'role': 'system', 'content': prompt},
+                    {'role': 'user', 'content': text}
+                ],
+            }
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+            headers = {'Authorization': f'Bearer {api_key}'}
+            async with session.post('https://api.openai.com/v1/chat/completions', json=payload, headers=headers) as response:
+                response_data = await response.json()
+                return response_data
+    except Exception as error:
+        print("Error processing chat:", error)
+        return None
 
-    st.write("# Welcome to Streamlit! 👋")
+def extract_text_from_pdf(uploaded_pdf):
+    pdf_reader = PdfReader(io.BytesIO(uploaded_pdf.read()))
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+    return text
 
-    st.sidebar.success("Select a demo above.")
+def display_data():
+    st.title("Scann PDF Demo - AUDITEX")
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+    # Prompts selection
+    prompt_options = {
+        "Conserto":"Vous êtes un assistant qui va extraire des textes qu ont vous fournie qui sont des textes extraies de pdf , les infofrmatiosn suivantes: la date, le numero de fatcure,l'objet de la prestation,le Libelle, le total HT et le siren.Montre moi le resultat sous forme de json",
+        # "Bulletins":"Vous êtes un assistant qui va extraire des textes qu ont vous fournie qui sont des textes extraies de pdf , les infofrmatiosn suivantes: Le salaire Brut Annuel du bulletin de salaire.Montre moi le resultat sous forme de json",
+        # "Dotations": "Prompt for option 3"
+    }
+    selected_option = st.selectbox("Select the prompt option:", list(prompt_options.keys()))
 
+    uploaded_pdf = st.file_uploader("Upload PDF", type=["pdf"])
 
-if __name__ == "__main__":
-    run()
+    if st.button("Process PDF"):
+        if uploaded_pdf:
+            pdf_text = extract_text_from_pdf(uploaded_pdf)
+            selected_prompt = prompt_options[selected_option]
+            response_data = asyncio.run(process_text(pdf_text, selected_prompt))
+            if response_data:
+                st.json(response_data['choices'][0]['message']['content'])
+            else:
+                st.error("Failed to process the text.")
+        else:
+            st.warning("Please upload a PDF file.")
+
+if __name__ == '__main__':
+    display_data()
